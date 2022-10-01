6eb3c3f74120970f856e55a418feedf766ae55aa
Peter luque <Peterluque21@gmail.com>
2022-10-01 17:14:34 -0400

    commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var myCar = new Car();
+            myCar.Make = "Dodge Charger";
+            myCar.Year = 2013;
+            myCar.Make = "R/T Plus";
+        }
+    }
+}
